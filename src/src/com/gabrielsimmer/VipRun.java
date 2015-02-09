b3942bcaf41fa93c3445bca44ad4ead8a2a80e8b
Gabriel Simmer <bladesimmer@gmail.com>
2015-02-09 09:06:33 -0800

    Create VipRun.java

---
+++ src/src/com/gabrielsimmer/VipRun.java
@@ -0,0 +1,40 @@
+package com.gabrielsimmer;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class VipRun implements CommandExecutor{
+
+	VipFun plugin;
+	ChatColor gC = ChatColor.GOLD; //Basically shortens ChatColor.GOLD to gC for easier access.
+	
+	public VipRun(VipFun passedPlugin){
+		this.plugin = passedPlugin;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		Player player = (Player) sender;
+		
+		if(!player.hasPermission("vipfun.vip")){
+			player.sendMessage(ChatColor.RED + "Sorry, you're not VIP! Use /buy to buy it!");
+		}
+		
+		else if (player.hasPermission("vipfun.run")){
+		
+		player.sendMessage(gC + "Enjoy the speed boost!");
+		
+		PotionEffect speed = PotionEffectType.SPEED.createEffect(2400, 1); //Create speed effect
+		
+		player.addPotionEffect(speed, true); //Give player effect
+		
+		}
+		
+		return false;
+	}
+}
